698b85a4e66104c06c5d40dde3c7451fdf3d62c7
Lilia Komova <lilak6051@gmail.com>
2023-03-18 23:25:19 +0300

    add hw

---
+++ hw_regular_expressions.py
@@ -0,0 +1,73 @@
+import csv
+from pprint import pprint
+from re import sub
+
+
+def open_file(filename):
+    with open(filename) as file:
+        contacts = csv.reader(file)
+        contacts_list = list(contacts)
+    return contacts_list
+
+
+def change_name(contacts_list):
+    name = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)' \
+           r'(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
+    name_new = r'\1\3\10\4\6\9\7\8'
+    contacts_list_new = list()
+    for value in contacts_list:
+        value_string = ','.join(value)
+        formatted_value = sub(name, name_new, value_string)
+        value_list = formatted_value.split(',')
+        contacts_list_new.append(value_list)
+    return contacts_list_new
+
+
+def change_number(contacts_list):
+    number = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
+             r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
+             r'(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
+    number_new = r'+7(\4)\8-\11-\14\15\17\18\19\20'
+    contacts_list_new = list()
+    for value in contacts_list:
+        value_string = ','.join(value)
+        formatted_value = sub(number, number_new, value_string)
+        value_list = formatted_value.split(',')
+        contacts_list_new.append(value_list)
+    return contacts_list_new
+
+
+def unification_duplicate(contacts_list):
+    for i in contacts_list:
+        for j in contacts_list:
+            if i[0] == j[0] and i[1] == j[1] and i is not j:
+                if i[2] in '':
+                    i[2] = j[2]
+                if i[3] in '':
+                    i[3] = j[3]
+                if i[4] in '':
+                    i[4] = j[4]
+                if i[5] in '':
+                    i[5] = j[5]
+                if i[6] in '':
+                    i[6] = j[6]
+    contacts_list_new = list()
+    for value in contacts_list:
+        if value not in contacts_list_new:
+            contacts_list_new.append(value)
+    return contacts_list_new
+
+
+def save_new_file(contacts_list):
+    with open("phonebook.csv", "w") as file:
+        datawriter = csv.writer(file, delimiter=',')
+        datawriter.writerows(contacts_list)
+
+
+if __name__ == '__main__':
+    cont = open_file('phonebook_raw.csv')
+    transform = change_name(cont)
+    transform = change_number(transform)
+    transform = unification_duplicate(transform)
+    pprint(transform)
+    save_new_file(transform)
