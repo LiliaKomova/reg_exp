20a6d68ddf7de59cfcaec22b547a65bcf6a6d6eb
Lilia Komova <lilak6051@gmail.com>
2023-03-19 15:01:02 +0300

    add modify hw

--- hw_regular_expressions.py
+++ hw_regular_expressions.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from re import sub
 
 
+# Читаем адресную книгу в формате CSV в список contacts_list:
 def open_file(filename):
     with open(filename) as file:
         contacts = csv.reader(file)
@@ -10,6 +11,8 @@ def open_file(filename):
     return contacts_list
 
 
+#  Поместить Фамилию, Имя и Отчество человека в поля
+#  lastname, firstname и surname соответственно.
 def change_name(contacts_list):
     name = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)' \
            r'(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
@@ -23,6 +26,8 @@ def change_name(contacts_list):
     return contacts_list_new
 
 
+#  Привести все телефоны в формат +7(999)999-99-99.
+#  Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999.
 def change_number(contacts_list):
     number = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
              r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
@@ -37,6 +42,7 @@ def change_number(contacts_list):
     return contacts_list_new
 
 
+#  Объединить все дублирующиеся записи о человеке в одну.
 def unification_duplicate(contacts_list):
     for i in contacts_list:
         for j in contacts_list:
@@ -58,16 +64,19 @@ def unification_duplicate(contacts_list):
     return contacts_list_new
 
 
+#  Сохранение, получившихся данных в другой файл
 def save_new_file(contacts_list):
     with open("phonebook.csv", "w") as file:
         datawriter = csv.writer(file, delimiter=',')
         datawriter.writerows(contacts_list)
 
-
-if __name__ == '__main__':
-    cont = open_file('phonebook_raw.csv')
-    transform = change_name(cont)
-    transform = change_number(transform)
-    transform = unification_duplicate(transform)
-    pprint(transform)
-    save_new_file(transform)
+try:
+    if __name__ == '__main__':
+        cont = open_file('phonebook_raw.csv')
+        transform = change_name(cont)
+        transform = change_number(transform)
+        transform = unification_duplicate(transform)
+        pprint(transform)
+        save_new_file(transform)
+except:
+    print('Error! Try again!')
